75b91f2440df1570f81c070bfd7941c501979423
kamasamikon <kamasamikon@qq.com>
2017-03-31 23:54:31 +0800

    use click and delete junk from xml files.

--- make.py
+++ make.py
@@ -9,8 +9,6 @@ client = pymongo.MongoClient()
 
 db = client.dyi
 
-wordcount = 0
-
 
 def addword(w, p, t):
     o = dict(_id=w, p=p, t=t)
@@ -26,8 +24,6 @@ def addword(w, p, t):
 
 
 def dotan(filepath):
-    global wordcount
-
     data = open(filepath).read()
     dic = xmltodict.parse(data)
 
@@ -36,22 +32,34 @@ def dotan(filepath):
         for word in words:
             w = word.get("word")
             p = word.get("phonetic") or ""
-            t = word.get("trans").replace("\r", "\n").replace("\n", " ").replace("  ", " ").strip()
-
+            t = word.get("trans") or ""
+            t = t.replace("\r", "\n").replace("\n", " ").replace("  ", " ").strip()
             addword(w, p, t)
-
-            wordcount += 1
 
             # output = "{0:>20} | {1:24} | {2}"
             # print(output.format(w,p,t))
 
-if __name__ == "__main__":
-    print("usage: make db/*.xml")
-    for filepath in sys.argv[1:]:
+import click
+
+@click.command()
+@click.option('--drop', default=True, help='Drop old database before update.')
+@click.option('--verbose', default=True, help='Show how many words inserted.')
+@click.argument('files', nargs=-1, type=str)
+def main(files, drop, verbose):
+    """Save words from FILES into MongoDB."""
+
+    if drop:
+        db.words.drop()
+
+    for f in files:
         try:
-            dotan(filepath)
+            dotan(f)
         except:
-            print("ERROR: ", filepath)
+            print("ERROR: ", f)
             traceback.print_exc()
 
-    print("wordcount:", wordcount)
+    if verbose:
+        print(db.words.find().count())
+
+if __name__ == '__main__':
+    main()
